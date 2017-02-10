7ecf1699ada3e8f1b6380fecac66393b38e6e9eb
Dennis Schmidt <dmrschmidt@gmail.com>
2017-02-10 20:48:14 +0100

    Initial Commit

---
+++ QRCode/QRCode.h
@@ -0,0 +1,19 @@
+//
+//  QRCode.h
+//  QRCode
+//
+//  Created by Dennis Schmidt on 10/02/2017.
+//  Copyright © 2017 Dennis Schmidt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QRCode.
+FOUNDATION_EXPORT double QRCodeVersionNumber;
+
+//! Project version string for QRCode.
+FOUNDATION_EXPORT const unsigned char QRCodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QRCode/PublicHeader.h>
+
+
