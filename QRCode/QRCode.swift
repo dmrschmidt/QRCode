1ddd44949cbc1a7f95eb04876c44b55539e35b2b
Dennis Schmidt <dmrschmidt@gmail.com>
2017-02-15 17:31:36 +0100

    initial implementation

--- QRCode/QRCode.swift
+++ QRCode/QRCode.swift
@@ -1,5 +1,93 @@
 import Foundation
 
-class QRCode {
-    
+/**
+ Generator for QRCode images.
+ */
+public struct QRCode {
+    /**
+     Amount of input error correction data to append to the final QR code.
+     Higher levels lead to larger image.
+
+     - **low**:      7%
+     - **medium**:   15%
+     - **quartile**: 25%
+     - **high**:     30%
+     */
+    public enum InputCorrection: String {
+        case low      = "L"
+        case medium   = "M"
+        case quartile = "Q"
+        case high     = "H"
+    }
+
+    /// QRCode data to be encoded.
+    public let data: Data
+
+    /// Output image foreground (actual code) color.
+    /// Defaults to black.
+    public var color = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+
+    /// Output image background color.
+    /// Defaults to white.
+    public var backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+
+    /// Desired Output image size.
+    public var size = CGSize(width: 200, height: 200)
+
+    /// Amount of input error correction to apply. Default is `.Low`.
+    public var inputCorrection = InputCorrection.low
+
+// MARK: - Initializers
+
+    public init(data: Data) {
+        self.data = data
+    }
+
+    public init?(string: String) {
+        guard let data = string.data(using: .isoLatin1) else { return nil }
+        self.data = data
+    }
+
+    public init?(url: URL) {
+        guard let data = url.absoluteString.data(using: .isoLatin1) else { return nil }
+        self.data = data
+    }
+
+    /// The QRCode's UIImage representation
+    public var image: UIImage? {
+        guard let ciImage = ciImage else { return nil }
+
+        let scaleX = size.width / ciImage.extent.size.width
+        let scaleY = size.height / ciImage.extent.size.height
+
+        let transformedImage = ciImage.applying(CGAffineTransform(scaleX: scaleX, y: scaleY))
+
+        return UIImage(ciImage: transformedImage)
+    }
+}
+
+// MARK: - Private
+
+extension QRCode {
+    fileprivate var ciImage: CIImage? {
+        guard let qrCodeImage = qrCodeImage,
+              let colorFilter = CIFilter(name: "CIFalseColor") else { return nil }
+
+        colorFilter.setDefaults()
+        colorFilter.setValue(qrCodeImage, forKey: "inputImage")
+        colorFilter.setValue(CIColor(cgColor: color.cgColor), forKey: "inputColor0")
+        colorFilter.setValue(CIColor(cgColor: backgroundColor.cgColor), forKey: "inputColor1")
+
+        return colorFilter.outputImage
+    }
+
+    private var qrCodeImage: CIImage? {
+        guard !data.isEmpty, let qrCodeFilter = CIFilter(name: "CIQRCodeGenerator") else { return nil }
+
+        qrCodeFilter.setDefaults()
+        qrCodeFilter.setValue(data, forKey: "inputMessage")
+        qrCodeFilter.setValue(inputCorrection.rawValue, forKey: "inputCorrectionLevel")
+
+        return qrCodeFilter.outputImage
+    }
 }
--- QRCodeTests/QRCodeSpec.swift
+++ QRCodeTests/QRCodeSpec.swift
@@ -6,6 +6,82 @@ import Nimble
 class QRCodeSpec: QuickSpec {
     override func spec() {
         describe("QRCode") {
+            var qrCode: QRCode!
+
+            describe("init(data:)") {
+                var data: Data!
+
+                beforeEach {
+                    data = "awesome".data(using: .isoLatin1)
+                    qrCode = QRCode(data: data)
+                }
+
+                it("creates a valid instance") {
+                    expect(qrCode).toNot(beNil())
+                }
+
+                it("encapsulates the passed data as is") {
+                    expect(qrCode.data).to(equal(data))
+                }
+            }
+
+            describe("init(string:)") {
+                var string: String!
+
+                beforeEach {
+                    string = "awesome"
+                    qrCode = QRCode(string: string)
+                }
+
+                it("creates a valid instance") {
+                    expect(qrCode).toNot(beNil())
+                }
+
+                it("encapsulates the passed string as ISO Latin1 converted data") {
+                    expect(qrCode.data).to(equal(string.data(using: .isoLatin1)))
+                }
+            }
+
+            describe("init(url:)") {
+                var url: URL!
+
+                beforeEach {
+                    url = URL(string: "http://awesome.com/amazing")
+                    qrCode = QRCode(url: url)
+                }
+
+                it("creates a valid instance") {
+                    expect(qrCode).toNot(beNil())
+                }
+
+                it("encapsulates the passed url as ISO Latin1 converted string data") {
+                    expect(qrCode.data).to(equal(url.absoluteString.data(using: .isoLatin1)))
+                }
+            }
+
+            describe("image") {
+                context("with empty or invalid data") {
+                    it("returns nil") {
+                        expect(QRCode(data: Data()).image).to(beNil())
+                    }
+                }
+
+                context("with valid data") {
+                    beforeEach {
+                        qrCode = QRCode(string: "foo bar")
+                    }
+
+                    it("returns a valid image") {
+                        expect(qrCode.image).to(beAnInstanceOf(UIImage.self))
+                    }
+
+                    it("resizes the image to desired dimensions") {
+                        qrCode.size = CGSize(width: 100, height: 100)
+                        expect(qrCode.image?.size.width).to(equal(100))
+                        expect(qrCode.image?.size.height).to(equal(100))
+                    }
+                }
+            }
         }
     }
 }
